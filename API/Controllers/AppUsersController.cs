ae771be0c461850e951a0959c8993e37bd196645
Phat Huynh <phat.huynh94@gmail.com>
2022-06-23 09:54:02 -0500

    Add project files.

---
+++ API/Controllers/AppUsersController.cs
@@ -0,0 +1,32 @@
+﻿using API.Data;
+using API.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class AppUsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public AppUsersController(DataContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<AppUser>>> GetUsers()
+        {
+            return await _context.AppUsers.ToListAsync();
+        }
+
+        // api/AppUsers/3
+        [HttpGet("{id}")]
+        public async Task<ActionResult<AppUser>> GetUser(int id)
+        {
+            return await _context.AppUsers.FindAsync(id);
+        }
+    }
+}
